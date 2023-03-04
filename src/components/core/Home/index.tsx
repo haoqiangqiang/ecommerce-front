0e4caa3ff0e13a54fd3f51a34397d70829227336
haoqiangqiang <hao_qiangqiang@topsec.com.cn>
2023-03-04 14:57:27 +0800

    初始化 redux

--- src/components/core/Home/index.tsx
+++ src/components/core/Home/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Layout from '../Layout'
 
 const Home = () => {
+    const state = useSelector(state => state)
+    console.log('state', state)
     return <Layout>Home</Layout>
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
 import Routes from './router';
+import store from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Routes />
+  <Provider store={store}>
+    <Routes />
+  </Provider>
 );
