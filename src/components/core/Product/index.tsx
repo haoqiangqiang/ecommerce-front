ea3c15c17634f7cde1744559d8f47d139981dbf4
haoqiangqiagn <826173335@qq.com>
2023-03-23 00:30:02 +0800

    展示商品详情

--- src/components/core/Product/index.tsx
+++ src/components/core/Product/index.tsx
@@ -2,23 +2,29 @@ import Layout from '../Layout'
 import react, { useEffect } from 'react'
 import { Col, Row } from 'antd'
 import { useParams } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getProductById } from '../../../service/app/product'
+import { State } from '../../../types'
+import ProductItem from '../ProductItem'
 
 const Product = () => {
 
     const dispatch = useDispatch()
 
+    const { product } = useSelector<State.AppState, State.ProductState>(state => state.product)
+
     const { productId } = useParams<{ productId: string }>()
 
     useEffect(() => {
-        dispatch(getProductById(productId))
+        dispatch(getProductById({ productId }))
     }, [])
 
     return (
         <Layout title='商品名称' subTitle="商品描述">
             <Row gutter={36}>
-                <Col span="18">left</Col>
+                <Col span="18">
+                    <ProductItem product={product.result} showViewProduct={false}></ProductItem>
+                </Col>
                 <Col span="6">right</Col>
             </Row>
         </Layout>
