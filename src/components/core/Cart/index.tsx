bb33bd0c34721815df73ef0a305e30f1ec343c28
haoqiangqiagn <826173335@qq.com>
2023-03-24 23:43:39 +0800

    支付

--- src/components/core/Cart/index.tsx
+++ src/components/core/Cart/index.tsx
@@ -54,7 +54,7 @@ const Cart = () => {
                         <TotalPrice cart={cart!} setTotalPrice={setTotalPrice} />
                     </Row>
                     <Row>
-                        <Pay />
+                        <Pay totalPrice={totalPrice} cart={cart!} address={address} />
                     </Row>
                 </Col>
             </Row>
--- src/components/core/Pay/index.tsx
+++ src/components/core/Pay/index.tsx
@@ -1,12 +1,35 @@
 import { Button } from 'antd'
-import react from 'react'
+import Axios from 'axios'
+import react, { FC } from 'react'
 import { Link } from 'react-router-dom'
+import { API } from '../../../config'
+import { AuthData } from '../../../interfaces/auth.interfaces'
+import { CartItem } from '../../../interfaces/cart.inerface'
 import { isAuth } from '../../../service/app/auth'
 
-const Pay = () => {
+interface Props {
+    totalPrice: number,
+    address: string,
+    cart: CartItem[]
+}
+
+const Pay: FC<Props> = ({ totalPrice, address, cart }) => {
+
+    const getPayUrl = () => {
+        Axios.post(`${API}/alipay`, {
+            totalAmount: totalPrice,
+            subject: '测试订单标题',
+            body: '测试订单描述',
+            products: cart.map((product) => ({ count: product.count, product: product._id })),
+            address,
+            userId: (isAuth() as AuthData).user._id
+        }).then(response => {
+            location.href = response.data.result
+        })
+    }
 
     const showButton = () => {
-        return isAuth() ? <Button>提交订单</Button> : <Button><Link to='/signin'></Link>登录</Button>
+        return isAuth() ? <Button onClick={getPayUrl}>提交订单</Button> : <Button><Link to='/signin'></Link>登录</Button>
     }
     return (
         <>{showButton()}</>
